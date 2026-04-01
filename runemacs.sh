436c0f9b68080e0284161762598517892d8b8835
William Brusa <william.brusa@studio.unibo.it>
2026-04-01 06:53:13 +0200

    fix: proper shell setup

--- runemacs.sh
+++ runemacs.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env sh
 
 guix shell -m emacs.scm -C --pure \
+    -E '^DISPLAY$' \
+    -E '^XAUTHORITY$' \
+    --share=/tmp/.X11-unix \
+    --expose=$XAUTHORITY \
     -- emacs -q -l init.el
