51dce6e82df538c728db50bafbc2bf6fb5f5d565
William Brusa <william.brusa@studio.unibo.it>
2026-04-01 19:11:59 +0200

    untrack init.el

--- init.el
+++
@@ -1,9 +0,0 @@
-;; TODO: add to a shared .el
-(let ((modules '("emacs-basic")))
-  (dolist (module modules)
-    (unless (file-exists-p (concat module ".el"))
-      (org-babel-tangle-file (concat module ".org")))
-    (load (concat module ".el"))))
-
-(emacs-basic/configure)
-(setq my/is-guix t)
